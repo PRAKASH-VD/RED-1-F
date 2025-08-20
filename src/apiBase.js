98217f5efb81eeb30600cd481dd31b82d3dc8d84
PRAKASH-VD <prakashdjp4@gmail.com>
2025-08-20 22:51:00 +0530

    x

--- src/apiBase.js
+++ src/apiBase.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const baseURL = import.meta?.env?.VITE_API_BASE_URL || "https://red1-1-0-0.onrender.com/api";
+
 const api = axios.create({
-  baseURL: "https://red1-1-0-0.onrender.com/api",
+  baseURL,
   withCredentials: true,
 });
 
